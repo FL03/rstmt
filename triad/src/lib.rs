8c1bd099d61ffe5e093d7330db9fec75d65afa31
Joe McCain III <jo3mccain@icloud.com>
2024-07-08 17:25:54 -0500

    initial commit

---
+++ triad/src/lib.rs
@@ -0,0 +1,15 @@
+/*
+    Appellation: rstopo <module>
+    Contrib: FL03 <jo3mccain@icloud.com>
+*/
+//! # triad
+//!
+//! This project focuses on providing concrete abstractions of musical objects discussed within the neo-Riemannian theory.
+#![cfg_attr(not(feature = "std"), no_std)]
+#![crate_name = "triad"]
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
+
+pub mod prelude {}
