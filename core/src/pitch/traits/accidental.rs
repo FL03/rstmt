20e6a528d30550515545c2c336fbcfb47b953895
FL03 <jo3mccain@icloud.com>
2025-12-31 22:34:43 -0600

    update accidental.rs
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- core/src/pitch/traits/accidental.rs
+++ core/src/pitch/traits/accidental.rs
@@ -73,19 +73,6 @@ macro_rules! accidental {
             }
         }
 
-        #[cfg(feature = "alloc")]
-        impl ::core::str::FromStr for $name {
-            type Err = $crate::error::Error;
-
-            fn from_str(s: &str) -> Result<Self, Self::Err> {
-                if s == stringify!($name) || s == $sym.to_string() {
-                    Ok(Self::default())
-                } else {
-                    Err($crate::error::Error::FromStrParseError)
-                }
-            }
-        }
-
         impl ::core::fmt::Debug for $name {
             fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
                 write!(f, "{}", self.symbol())
@@ -188,10 +175,10 @@ impl core::str::FromStr for Natural {
     type Err = crate::error::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s.to_lowercase() == "natural" || s == '♮'.to_string() || s == "" {
+        if s.is_empty() || s.to_lowercase() == "natural" || s == "♮" {
             Ok(Natural)
         } else {
-            Err(crate::error::Error::FromStrParseError)
+            Err(anyhow::anyhow!("Unable to parse a natural note : {}", s).into())
         }
     }
 }
@@ -207,3 +194,29 @@ impl core::fmt::Display for Natural {
         write!(f, "{}", self.symbol())
     }
 }
+
+    #[cfg(feature = "alloc")]
+    impl ::core::str::FromStr for Flat {
+        type Err = crate::error::Error;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            if s.to_lowercase() == "flat" || s == "♭" || s == "b" {
+                Ok(Self::default())
+            } else {
+                Err(anyhow::anyhow!("Invalid accidental string: {}", s).into())
+            }
+        }
+    }
+
+    #[cfg(feature = "alloc")]
+    impl ::core::str::FromStr for Sharp {
+        type Err = crate::error::Error;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            if s.to_lowercase() == "sharp" || s == "♯" || s == "#" {
+                Ok(Self::default())
+            } else {
+                Err(anyhow::anyhow!("Invalid accidental string: {}", s).into())
+            }
+        }
+    }
