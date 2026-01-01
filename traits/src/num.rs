454219134e0e258bef57e64e83745755c6b2640b
FL03 <jo3mccain@icloud.com>
2026-01-01 10:31:43 -0600

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- traits/src/num.rs
+++ traits/src/num.rs
@@ -6,6 +6,8 @@
 use crate::{PitchMod, PyMod};
 use num_traits::{FromPrimitive, One, ToPrimitive, Zero};
 
+/// The [`OrderedNum`] trait defines the behavior of ordered numerical types, providing methods 
+/// for establishing the sign of a number and other fundamental numerical operations.
 pub trait OrderedNum
 where
     Self: PartialEq + PartialOrd + One + Zero,
